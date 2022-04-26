f8e1e953462deca8da0a85031bbdb8ee56904f04
etosphere <etosphere@outlook.com>
2022-04-25 22:24:31 -0400

    Add jianzi font and vertical textbox

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,10 @@
+import { Container, Flex, Text } from '@chakra-ui/react'
 import { NextPage } from 'next'
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Qino</title>
         <meta
@@ -13,6 +13,24 @@ const Home: NextPage = () => {
         />
         <link rel='icon' href='/favicon.ico' />
       </Head>
+      <Container maxHeight='86%' maxW='1280px' m='auto'>
+        <Flex className='jianzipu' flexDirection='column'>
+          <Container
+            overflowX='scroll'
+            maxHeight='86%'
+            p={16}
+            letterSpacing='1px'
+            style={{ writingMode: 'vertical-rl', textOrientation: 'upright' }}
+          >
+            <Text fontSize='32px'>
+              {'\uE005 = \uE000\uE001\uE002\uE003\uE004'}
+            </Text>
+            <Text fontSize='32px'>
+              {'\uE000\uE001\uE002\uE003\uE004 Lorem Ipsum!'}
+            </Text>
+          </Container>
+        </Flex>
+      </Container>
     </div>
   )
 }
