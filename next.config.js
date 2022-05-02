843f414f62a2ac69c1a1be1787c56ddd0d573074
etosphere <etosphere@outlook.com>
2022-05-02 16:20:56 -0400

    Set script for static HTML export

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
 
-module.exports = nextConfig
+const isProd = process.env.NODE_ENV === 'production'
+
+module.exports = {
+  reactStrictMode: true,
+  assetPrefix: isProd ? '/your-github-repo-name/' : ''
+}
